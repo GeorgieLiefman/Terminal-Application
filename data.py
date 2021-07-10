2b86addd17344966bafafe30b5b331be7bf0d396
GeorgieLiefman <gkb.liefman@gmail.com>
2021-07-10 23:41:50 +1000

    Created separate file to store data

---
+++ data.py
@@ -0,0 +1,23 @@
+# Dictionary for cows's status
+status = {
+    "Name": "", 
+    "Type": "", 
+    "Age": 0, 
+    "Hunger": 0,
+    "Happiness": 100, 
+    "Owned Toys": []
+}
+
+# Dictionary for cow's toys
+toy_dictionary = {
+    "Highland": ["bagpipes", "Loch Ness monster", "rugby ball"], 
+    "Texas Longhorn": ["guitar", "Trump", "tumbleweed"], 
+    "Limousin": ["bike", "skiis", "Effiel Tower"], 
+}
+
+# Dictionary for cow's food
+food_dictionary = {
+    "Highland": ["haggis", "shortbread", "bacon butty"], 
+    "Texas lLnghorn": ["barbeque", "chilli", "anything fried"], 
+    "Limousin": ["baguette", "cigarette", "croissant"], 
+}
--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-# Dictionary for cows's status
-status = {
-    "Name": "", 
-    "Type": "", 
-    "Age": 0, 
-    "Hunger": 0,
-    "Happiness": 100, 
-    "Owned Toys": []
-}
-
-# Dictionary for cow's toys
-toy_dictionary = {
-    "Highland": ["bagpipes", "Loch Ness monster", "rugby ball"], 
-    "Texas Longhorn": ["guitar", "Trump", "tumbleweed"], 
-    "Limousin": ["bike", "skiis", "Effiel Tower"], 
-}
-
-# Dictionary for cow's food
-food_dictionary = {
-    "Highland": ["haggis", "shortbread", "bacon butty"], 
-    "Texas lLnghorn": ["barbeque", "chilli", "anything fried"], 
-    "Limousin": ["baguette", "cigarette", "croissant"], 
-}
+from data import status, toy_dictionary, food_dictionary
 
 # Function for user to select breed of cow they want
 def starting_choices():
