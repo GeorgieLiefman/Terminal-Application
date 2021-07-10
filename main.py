2d116c2bd069a60709a83401a49032462ef1ff62
GeorgieLiefman <gkb.liefman@gmail.com>
2021-07-10 12:43:37 +1000

    Setup dictionaries for cow's status, toys and food

---
+++ main.py
@@ -0,0 +1,23 @@
+# Dictionary for cows's status
+status = {
+    "Name": "", 
+    "Type": "", 
+    "Age": 0, 
+    "Hunger": 0,
+    "Happiness": 100, 
+    "Owned Toys": []
+}
+
+# Dictionary for cow's toys
+toy_list = {
+    "highland cow": ["bagpipes", "Loch Ness monster", "rugby ball"], 
+    "texas longhorn cow": ["guitar", "Trump", "tumbleweed"], 
+    "limousin cow": ["bike", "skiis", "Effiel Tower"], 
+}
+
+# Dictionary for cow's toys
+toy_list = {
+    "highland cow": ["haggis", "shortbread", "bacon butty"], 
+    "texas longhorn cow": ["barbeque", "chilli", "anything fried"], 
+    "limousin cow": ["baguette", "cigarette", "croissant"], 
+}
