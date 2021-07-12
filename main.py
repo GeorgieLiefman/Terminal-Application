113d9f59245facbf68659f09995b84200ec60852
GeorgieLiefman <gkb.liefman@gmail.com>
2021-07-12 23:56:12 +1000

    Amended selection process of cow to improve user experience

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from data import status, toy_dictionary, food_dictionary, menu_dictionary, breed_dictionary
+from data import status, toy_dictionary, food_dictionary, breed_dictionary
 
 # Function for user to select breed of cow they want and name said cow
 def starting_choices():
     cow_choice = ""
 
-    breed_list = list(food_dictionary.keys())
+    breed_list = list(breed_dictionary.keys())
     #validate user input
     while cow_choice not in breed_list:
-        print("Here are the breeds availabe for adoption. Make sure you pick something from the list!")
-        for option in breed_list:
-            print(option)
-        cow_choice = input("Which type of cow would you like to adopt? ")
+        print("""Here are the breeds availabe for adoption. 
+Make sure you type the letter which corresponds with the breed you want!""")
+        for key in breed_list:
+            print(key + ":\t" + breed_dictionary[key]["text"])
+        cow_choice = input("Which type of cow would you like to adopt? ").upper()
+        print(" ")
 
     # User input to name pet
     name = input(f"Woohoo! You've adopt a {cow_choice} cow as your pet. What will you name them? ")
     print(f"{name} is a great name for such a cool {cow_choice} cow!")
+    print(" ")
+
+starting_choices()
 
 
 # Print menu
