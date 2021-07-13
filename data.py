4e9f072f6f478c1d86db5bc905ff4246801ae4c8
GeorgieLiefman <gkb.liefman@gmail.com>
2021-07-14 00:07:59 +1000

    fixed errors relating to breed display in status and related text output

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
 # Dictionary for cows's status
-status = {
-    "Name": "", 
-    "Type": "", 
-    "Age": 0, 
-    "Hunger": 0,
-    "Happiness": 100, 
-    "Owned Toys": []
+cow = {
+    "name": "", 
+    "breed": "", 
+    "age": 0, 
+    "hunger": 0,
+    "happiness": 100, 
+    "toys": []
 }
 
 # Dictionary for cow breeds for user to select from
@@ -27,13 +27,4 @@ food_dictionary = {
     "Highland": ["haggis", "shortbread", "bacon butty"], 
     "Texas Longhorn": ["barbeque", "chilli", "anything fried"], 
     "Limousin": ["baguette", "cigarette", "croissant"], 
-}
-
-# Dictionary for menu options
-menu_dictionary = {
-        "F": {"function": feed_cow, "text": "Feed " + cow["name"] + "."},
-        "T": {"function": new_toys, "text": "Get a new toy for " + cow["name"] + "."},
-        "P": {"function": play_toys, "text": "Have " + cow["name"] + " play with their toys."},
-        "R": {"function": rps_game, "text": "Play Rock, Paper, Scissors against " + cow["name"] + "!"},
-        "Q": {"function": quit_app, "text": "Stop playing with " + cow["name"] + " and quit the application."}
 }
--- main.py
+++ main.py
@@ -1,58 +1,24 @@
-from data import status, toy_dictionary, food_dictionary, breed_dictionary
+from data import cow, toy_dictionary, food_dictionary, breed_dictionary
 
 # Function for user to select breed of cow they want and name said cow
 def starting_choices():
-    cow_choice = ""
+    cow["breed"] = ""
 
     breed_list = list(breed_dictionary.keys())
     #validate user input
-    while cow_choice not in breed_list:
+    while cow["breed"] not in breed_list:
         print("""Here are the breeds availabe for adoption. 
 Make sure you type the letter which corresponds with the breed you want!""")
         for key in breed_list:
             print(key + ":\t" + breed_dictionary[key]["text"])
-        cow_choice = input("Which type of cow would you like to adopt? ").upper()
+        cow["breed"] = input("Which type of cow would you like to adopt? ").upper()
         print(" ")
 
     # User input to name pet
-    name = input(f"Woohoo! You've adopt a {cow_choice} cow as your pet. What will you name them? ")
-    print(f"{name} is a great name for such a cool {cow_choice} cow!")
+    cow["name"] = input("Woohoo! You've adopt a cow as your pet. What will you name them? ")
+    print(cow["name"] + " is a great name for such a cool cow!")
     print(" ")
 
 starting_choices()
 
-
-# Print menu
-    def printMenu(menuOptions):
-        optionKeys = list(menuOptions.keys())
-
-        print("Here are your options:")
-        print("-------")
-        for key in optionKeys:
-            print(key + ":\t" + menuOptions[key]["text"])
-
-
-
-
-def main():
-
-    starting_choices()
-
-    printMenu(menuOptions)
-
-main() 
-
-# Function to play with cow's toys
-
-# Function to get new toys for user's cow
-
-# Function to quit simulator 
-
-# Function to feed user's cow
-
-# Function to play rock, paper, scissors 
- 
-# Function to print out the updated status of the user's cow each "day"
-
-
-# Function for the main menu game loop
+print(cow)
