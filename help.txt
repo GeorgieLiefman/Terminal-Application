7febea79817341fd59c57b24f24275bc768472d2
GeorgieLiefman <gkb.liefman@gmail.com>
2021-07-17 20:55:23 +1000

    Linked help file to bash wrapper

---
+++ help.txt
@@ -0,0 +1,16 @@
+Steps to install application:
+1) Have downloaded the python code and bash scripts for the program.
+2) Install Python 3 if it is not already on your device.
+3) Install the virtual environment and the module ‘Cowsay 4.0’. 
+4) Create a virtual environment. One of the bash scripts will complete step 3 and 4 if you allow it to by entering the required argument.
+5) Activate your virtual environment in the terminal by writing ‘source venv/bin/activate’. 
+6) Run the wrapper.sh script in the terminal and enter the argument to run the application.
+
+Dependencies:
+The application depends on Python 3 and the package ‘Cowsay 4.0’ to run. It’s necessary for the user's device to have these installed to run the application.
+
+System/hardware requirements:
+Whilst the program doesn’t require any special systems/hardware, it’s necessary that at a minimum your device can install/run the mentioned dependencies. Any device which is not capable of this cannot run the application.
+
+Different features:
+In this application users will adopt, care for and play with a virtual cow through the features displayed in the menu. These features include: feeding them, giving your cow a new toy, walking them and having them play with its toys. After an activity has been completed the cow’s updated stats will be displayed. These stats include its age, owned toys, happiness and hunger levels. 
--- wrapper.sh
+++ wrapper.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-echo "My program relies on virtual environments and the 3rd party module (Cowsay 4.0) to run. As such, if you do not have either of these installed in your device can I please run a script to install them? If you agree please enter 'yes' as the first argument."
+echo "To read the help file for this program please enter 'help' as the first argument."
 echo ""
-echo "If you've just installed venv and the module or already had them installed and want to run the application please enter 'run' as the second argument."
+echo "My program relies on virtual environments and the 3rd party module (Cowsay 4.0) to run. As such, if you do not have either of these installed in your device can I please run a script to install them? If you agree please enter 'yes' as the second argument."
 echo ""
+echo "If you've just installed venv and the module or already had them installed and want to run the application please enter 'run' as the third argument."
+echo ""
+echo "Please activate your vitual environment before attempting to run this program. To activate it type ‘source venv/bin/activate’ into the terminal"
 echo "I am running a script to check you have the correct installation of Python to run the application."
 sh environment.sh
 
-if [[ $1 == "yes" ]]
+if [[ $1 == "help" ]]
+    then
+        cat help.txt
+fi
+
+if [[ $2 == "yes" ]]
     then
         sh installation.sh
 fi
 
-if [[ $2 == "run" ]]
+if [[ $3 == "run" ]]
     then
         python3 main.py
 fi
+
